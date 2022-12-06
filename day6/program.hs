1bfa7488906b5ae441d26c58faac231db13249be
Tim Löfström <tim.lofstrom.danielsson@lm.se>
2022-12-06 14:18:10 +0100

    update

--- day6/program.hs
+++ day6/program.hs
@@ -1,10 +1,13 @@
+import Data.List (nub)
 
-
-toCharArray :: String -> [Char]
-toCharArray x = x
+calc numbers [] list = calc numbers (take numbers list) (drop 1 list)
+calc numbers window rest =
+  if length (nub window) == numbers
+    then numbers
+    else calc numbers (take numbers rest) (drop 1 rest) + 1
 
 main :: IO ()
 main = do
-  fileData <- readFile "input.txt"
-  let sum = toCharArray fileData
-  print sum
+  line <- readFile "input.txt"
+  print (calc 4 [] line)
+  print (calc 14 [] line)
--- day6/program.py
+++ day6/program.py
@@ -1,40 +1,13 @@
+parts = list(open("input.txt").readline())
 
-line = ""
-with open("input.txt") as file:
-    for temp in file:
-        line = temp
+def find(numbers):
+    for i in range(0,len(parts)):
+        letters = set()
+        for j in range(0,numbers):
+            letters.add(parts[i+j])
+        if len(letters) == numbers:
+            i += numbers
+            return i
 
-parts = list(line)
-
-
-i = 0
-
-while i < len(parts):
-
-
-    list = []
-    list.append(parts[i])
-    list.append(parts[i+1])
-    list.append(parts[i+2])
-    list.append(parts[i+3])
-    list.append(parts[i+4])
-    list.append(parts[i+5])
-    list.append(parts[i+6])
-    list.append(parts[i+7])
-    list.append(parts[i+8])
-    list.append(parts[i+9])
-    list.append(parts[i+10])
-    list.append(parts[i+11])
-    list.append(parts[i+12])
-    list.append(parts[i+13])
-    myset = set(list)
-
-    print(myset)
-    
-    if len(myset) == 14:
-        i += 14
-        break
-    i += 1
-
-
-print(i)
+print(find(4))
+print(find(14))
